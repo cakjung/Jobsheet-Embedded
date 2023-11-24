0effecb05ff47ca74e22e9b0e92e63806fe2de50
cakjung <wedoung87@gmail.com>
2023-11-24 19:38:09 +0700

    24 Nov 2023

---
+++ Jobsheet 2/Job 2-B_2/DHT_BUZZER/DHT_BUZZER.ino
@@ -0,0 +1,34 @@
+#include <DHT.h>
+
+#define DHT_PIN 4   // Pin yang digunakan untuk sensor suhu DHT
+#define LED_PIN 5   // Pin yang digunakan untuk LED Merah
+#define BUZZER_PIN 18 // Pin yang digunakan untuk Buzzer
+
+DHT dht(DHT_PIN, DHT11); // Inisialisasi sensor DHT
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);     // Mengatur pin LED sebagai OUTPUT
+  pinMode(BUZZER_PIN, OUTPUT); // Mengatur pin Buzzer sebagai OUTPUT
+
+  Serial.begin(9600);
+  dht.begin(); // Memulai sensor DHT
+}
+
+void loop() {
+  float temperature = dht.readTemperature(); // Membaca suhu dari sensor DHT
+
+  Serial.print("Suhu: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  if (temperature >= 30.0) {
+    digitalWrite(LED_PIN, HIGH);     // Menyalakan LED Merah
+    tone(BUZZER_PIN, 1000, 100);      // Memainkan nada dengan frekuensi 1000Hz selama 100ms (beep)
+    delay(100); // Memberi jeda agar buzzer bisa berbunyi dengan interval
+  } else {
+    digitalWrite(LED_PIN, LOW); // Mematikan LED Merah
+    noTone(BUZZER_PIN);          // Mematikan Buzzer
+  }
+
+  delay(100); // Memberi jeda selama 1 detik (1000ms) sebelum membaca suhu lagi
+}
